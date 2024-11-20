d990079efb5eba587380ea8cb25890b43f0611f1
Ximena <moreraximena36@gmail.com>
2024-11-19 20:46:35 -0500

    se envian cambios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBank, faBuilding, faPencil, faTag, faTrash, faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPhone)
+library.add(faUser)
+library.add(faTrash)
+library.add(faPencil)
+library.add(faTag)
+library.add(faBuilding)
+library.add(faBank)
+library.add(faPlus)
+
 
 createApp(App).use(router).mount('#app')
